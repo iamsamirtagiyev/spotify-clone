e598ef5f8b82bb5d2dc250cec9c07e705c26bb85
Samir <121564530+iamsamirtagiyev@users.noreply.github.com>
2024-08-08 22:19:36 +0400

    Update context.jsx

--- src/context.jsx
+++ src/context.jsx
@@ -4,7 +4,7 @@ export const Context = createContext();
 
 export const Provider = ({ children }) => {
   const clientId = "d3df23ab227f461486d6012895eedefe";
-  const redirectURL = 'http://localhost:5173/' || 'https://spotify-iaye.vercel.app';
+  const redirectURL = 'https://spotify-iaye.vercel.app/'; //ve ya http://localhost:5173/
   const apiURL = "https://accounts.spotify.com/authorize";
   const scope = [
     "user-read-email",
